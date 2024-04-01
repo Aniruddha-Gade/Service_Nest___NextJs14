8aefe0134f3f946cd50befa0ca6ffb8f235bb0d8
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-01 20:57:11 +0530

    add: services page

---
+++ app/services/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='text-6xl h-screen flex items-center justify-center'>
+            Our  Services
+        </div>
+    );
+}
+
+export default Services;
