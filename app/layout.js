a5f402b7a62c1889b0d3a5b86213136674a9d2b9
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-01 19:42:48 +0530

    add: footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 const inter = Outfit({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <div className="">
           <Header />
           {children}
+          <Footer />
         </div>
       </body>
     </html>
