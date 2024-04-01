67fdf3c0bfac8e18ceec9e6f75bbc45d30b37290
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-01 20:56:38 +0530

    add: about page

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='text-6xl h-screen flex items-center justify-center'>
+            About page
+        </div>
+    );
+}
+
+export default About;
