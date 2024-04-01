463bc17eaab17b92baa4ea2c0e9f758970557791
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-01 23:23:05 +0530

    add: login/sign up in navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,15 +3,31 @@
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Image from "next/image";
 
 const Header = () => {
+
+  const { data } = useSession();
+
+  useEffect(() => {
+    console.log("user data = ", data)
+  }, [data])
+
   return (
     <div className=" p-2 px-6 md:px-16 shadow-md">
       <div className="flex items-center justify-between gap-8 ">
         {/* website logo */}
         <Link href='/' className='flex gap-1 items-center justify-center '>
-          <img src="/assets/logo/smart-home-logo.jpg" alt="" width={70} height={70} />
+          <Image src="/assets/logo/smart-home-logo.jpg" alt="" width={70} height={70} />
           <p className="text-lg sm:text-2xl font-bold">Service Nest</p>
         </Link>
 
@@ -35,8 +51,34 @@ const Header = () => {
             About Us
           </Link>
         </div>
+
         <div>
-          <Button onClick={() => signIn('descope')}>Login / Sign Up</Button>
+          {data?.user ?
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Image src={data?.user?.image}
+                  alt='user'
+                  width={40}
+                  height={40}
+                  className='rounded-full'
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link href={'/mybooking'}>My Booking</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem>
+
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            :
+
+            <Button onClick={() => signIn('descope')}>Login / Sign Up</Button>
+          }
         </div>
       </div>
     </div>
