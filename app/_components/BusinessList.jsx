c1c97265b7322d63c3f6a12faa214bb5e7ee7b9b
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-02 01:58:14 +0530

    add: business details page and search page

--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -13,20 +13,20 @@ function BusinessList({ businessList, title }) {
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5 '>
                 {businessList.length > 0 ? businessList.map((business, index) => (
                     <Link href={'/details/' + business.id}
-                        key={ business.id} className='rounded-lg shadow-lg cursor-pointer hover:shadow-xl hover:scale-105 duration-300 transition-all ease-in-out'>
+                        key={business.id} className='rounded-lg shadow-lg cursor-pointer hover:shadow-xl hover:scale-105 duration-300 transition-all ease-in-out flex flex-col h-full'>
                         <Image src={business?.images[0].url}
                             alt={business.name}
                             width={500}
                             height={200}
                             className='h-[150px] md:h-[200px] object-cover rounded-lg'
                         />
-                        <div className='flex flex-col justify-between p-3 gap-1'>
+                        <div className='flex flex-col justify-around h-full p-3 gap-1'>
                             <h2 className='p-1 w-fit bg-purple-200 text-primary rounded-full px-2 text-[12px]'>
                                 {business?.category?.name}
                             </h2>
                             <h2 className='font-bold text-lg'>{business.name}</h2>
                             <h2 className='text-primary'>{business.contactPerson}</h2>
-                            <h2 className='text-gray-500 text-sm'>{business.address}</h2>
+                            <h2 className='text-gray-500 text-sm'>{business.address.split(" ").slice(0, 15).join(" ")}</h2>
                             <Button className="rounded-lg mt-3">Book Now</Button>
                         </div>
                     </Link>
