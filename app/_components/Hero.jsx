83e61bf657f2faab2ff99bd0560ace5f0e3e16fb
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-02 14:25:02 +0530

    update: home page UI

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,26 +1,40 @@
+import React from "react";
+import { motion } from "framer-motion"; // Import motion from framer-motion for animations
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex items-center gap-3 flex-col justify-center pt-14 pb-7">
-      <h2 className="font-bold text-[46px] text-center">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }} // Initial animation state
+      animate={{ opacity: 1, y: 0 }} // Animation when component is mounted
+      transition={{ duration: 0.5 }} // Animation duration
+      className="flex items-center gap-3 flex-col justify-center pt-24 pb-7 bg-gradient-to-r from-blue-500 to-purple-500"
+    >
+      <h2 className="font-bold text-[46px] text-center text-white">
         Find Home
         <span className="text-primary"> Service/Repair</span>
-        <br></br> Near You
+        <br />
+        Near You
       </h2>
+
       <h2 className="text-xl text-center text-gray-400">
         Explore Best Home Service & Repair near you
       </h2>
-      <div className="mt-4 flex gap-4 items-center">
-        <Input placeholder="Search" className="rounded-full md:w-[350px]" />
-        <Button className="rounded-full h-[46px]">
-          <Search className="h-4 w-4" />
-        </Button>
+
+      <div className="mt-4 flex gap-4 items-center pb-24">
+        <Input
+          placeholder="Search"
+          className="rounded-full md:w-[350px] bg-white text-black" // Change input styles
+        />
+        <button className="rounded-full h-[46px] w-[3rem] flex justify-center items-center bg-white text-black">
+          {" "}
+          {/* Change button styles */}
+          <Search className="h-4 w-4 bg-white" />
+        </button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
