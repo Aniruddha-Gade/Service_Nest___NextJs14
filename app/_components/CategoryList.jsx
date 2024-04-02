7d73021ab235514e4fa953754c30ff8a92d8238f
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-02 15:23:03 +0530

    add: CategoryList

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -6,7 +6,7 @@ function CategoryList({ categoryList }) {
   console.log("categoryList =", categoryList);
   return (
     <div
-      className="mx-4 py-10 md:mx-22 lg:mx-52 grid grid-cols-3
+      className="mx-4 py-1 md:mx-22 lg:mx-52 grid grid-cols-3
     md:grid-cols-4 lg:grid-cols-6 gap-4"
     >
       {categoryList.length > 0
@@ -14,11 +14,8 @@ function CategoryList({ categoryList }) {
             <Link
               href={"/search/" + category.name}
               key={index}
-              className={`flex flex-col items-center
-             justify-center gap-2
-             bg-purple-50 p-5 rounded-lg
-             cursor-pointer hover:scale-110 transition-all ease-in-out
-             `}
+              className={`flex flex-col items-center justify-center gap-2
+             bg-purple-50 p-5 rounded-lg cursor-pointer hover:scale-110 transition-all ease-in-out hover:bg-purple-100`}
             >
               <div className="animate-pulse">
                 <Image
