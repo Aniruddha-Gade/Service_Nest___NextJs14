d431b5c1582dbf2fbd060c95fa7f2a3a104ee115
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-02 15:23:19 +0530

    add: Header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -23,29 +23,29 @@ const Header = () => {
   }, [data])
 
   return (
-    <div className="px-6 md:px-16 shadow-md">
+    <div className="px-6 md:px-16 shadow-md text-white bg-slate-100 md:bg-transparent">
       <div className="flex items-center justify-between gap-8 ">
         {/* website logo */}
         <Link href='/' className='flex gap-1 items-center justify-center '>
           <Image src="/assets/logo/smart-home-logo.png" alt="" width={70} height={70} />
-          <p className="text-lg sm:text-2xl font-bold">Service Nest</p>
+          <p className="text-lg sm:text-2xl font-bold text-black md:text-white">Service Nest</p>
         </Link>
 
         <div className="md:flex items-center gap-6 hidden">
           <Link href='/'
-            className="hover:scale-105 hover:text-primary
+            className="hover:scale-105 hover:text-purple-400
                 cursor-pointer"
           >
             Home
           </Link>
           <Link href='/services'
-            className="hover:scale-105 hover:text-primary
+            className="hover:scale-105 hover:text-purple-400
                 cursor-pointer"
           >
             Services
           </Link>
           <Link href='/about'
-            className="hover:scale-105 hover:text-primary
+            className="hover:scale-105 hover:text-purple-400
                 cursor-pointer"
           >
             About Us
